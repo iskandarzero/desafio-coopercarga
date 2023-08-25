06e38da08b40cc0295b9eb5e403bb15a85db5d2a
iskandarzero <riderzerotg@gmail.com>
2023-08-25 06:15:10 -0300

    configure react router

--- coopercarga-marketplace/src/App.js
+++ coopercarga-marketplace/src/App.js
@@ -1,9 +1,21 @@
 import Home from "./pages/Home";
+import Construction from "./pages/Construction";
+import NavBar from "./components/NavBar"
+import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
+import './App.css'
 
 function App() {
   return (
     <>
-      <Home />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/help" element={<Construction />} />
+        <Route path="/jobs" element={<Construction />} />
+        <Route path="/login" element={<Construction />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
