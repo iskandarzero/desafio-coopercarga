73a05a0fcfc0be14b01b2b740366e1c1ad56c1a3
iskandarzero <riderzerotg@gmail.com>
2023-08-25 06:13:16 -0300

    feat: Footer component

---
+++ coopercarga-marketplace/src/components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+  return (
+    <footer class="py-3 border-top border-dark bg-light mt-4">
+      <div class="container text-center">
+        <small>Desenvolvido por </small>
+        <a href="https://portifolio-ten-dusky.vercel.app/" target="_blank" rel="noreferrer">Isaac Farias</a>
+      </div>
+    </footer>
+  );
+}
